489f5f373a73aa0726ae69a6f6c77386b4b0b6ce
MZun <zun.mateusz@gmail.com>
2019-02-03 15:18:06 +0100

    Add string to bits, update gitignore with __pycache__

--- basic/one_time_pad/basic.py
+++ basic/one_time_pad/basic.py
@@ -9,17 +9,25 @@ def convert_to_bits(n, pad):
             else:
                 result = [1] + result
             n = int(n/2)
-            print(n)
     while len(result) < pad:
         result = [0] + result
 
     return result
 
+def string_to_bits(s):
+    result = []
+    for c in s:
+        result = result + convert_to_bits(ord(c), 7 )
+    return result
+
+def display_bits(bits):
+    print("".join(str(x) for x in bits))
+
 if __name__ == "__main__":
     print("one_time_pad");
 
-    converted = convert_to_bits(254,8)
-
+    converted = convert_to_bits(7,8)
+    display_bits(converted)
 
     print("".join(map(str,converted)))
 
